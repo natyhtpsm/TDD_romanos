9a7baefcdf76c420fee1201104c66f7efe9004f5
Nathália P. Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-04-24 16:44:49 -0300

    Feat: Test case 1
    The first testcase tests if the roman numbers are being recognized

--- romanos.cpp
+++ romanos.cpp
@@ -1,6 +1,37 @@
 #include "romanos.hpp"
-int romanos_para_decimal(char const * num_romano)
-{
-  return 0; 
+
+int valor_romano(char algarismo) {
+  switch(algarismo) {
+    case 'I':
+      return 1;
+    case 'V':
+      return 5;
+    case 'X':
+      return 10;
+    case 'L':
+      return 50;
+    case 'C':
+      return 100;
+    case 'D':
+      return 500;
+    case 'M':
+      return 1000;
+    default:
+      return 0; 
+  }
 }
- 
+
+int romanos_para_decimal(const char* num_romano) {
+  int resultado = 0;
+  int i = 0; 
+  while (num_romano[i]) {
+    if (valor_romano(num_romano[i]) < valor_romano(num_romano[i + 1])) {
+        resultado -= valor_romano(num_romano[i]);
+      } else {
+          resultado += valor_romano(num_romano[i]);
+      }
+      i++;
+    }
+    
+  return resultado;
+}
--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -3,18 +3,28 @@
 #include "romanos.hpp"
 
 
-TEST_CASE( "Numeros romanos - algarismos únicos", "[romanos]" ) {
+TEST_CASE( "Numeros romanos - algarismos ï¿½nicos", "[romanos]" ) {
     REQUIRE( romanos_para_decimal("I") == 1 );
     
     REQUIRE( romanos_para_decimal("V") == 5 );
 
-    
+    REQUIRE( romanos_para_decimal("X") == 10 );
+
+    REQUIRE( romanos_para_decimal("L") == 50 );
+
+    REQUIRE( romanos_para_decimal("C") == 100 );
+
+    REQUIRE( romanos_para_decimal("D") == 500 );
+
+    REQUIRE( romanos_para_decimal("M") == 1000 );
+
+      
 }
 
 
 
 
-TEST_CASE( "Numeros romanos - algarismos inválidos", "[romanos]" ) {
+TEST_CASE( "Numeros romanos - algarismos invï¿½lidos", "[romanos]" ) {
     REQUIRE( romanos_para_decimal("G") == -1 );
     
     REQUIRE( romanos_para_decimal("i") == -1 );

56c1449dedbca8d16dbe8f7b7355aad2c2587f60
Nathália P. Assis <119613135+natyhtpsm@users.noreply.github.com>
2024-04-27 18:59:42 -0300

    Update: romanos conversor algorithm
    now algorithm works to detect specific cases easily

--- romanos.cpp
+++ romanos.cpp
@@ -1,83 +1,40 @@
-#include "./romanos.hpp"
 #include <string>
-#include <iostream>
+#include <vector>
+#include "./romanos.hpp"
 
-int valor_romano(char algarismo) {
-  switch (algarismo) {
-    case 'I':
-      return 1;
-    case 'V':
-      return 5;
-    case 'X':
-      return 10;
-    case 'L':
-      return 50;
-    case 'C':
-      return 100;
-    case 'D':
-      return 500;
-    case 'M':
-      return 1000;
-    default:
-      return -1;
-  }
-}
+std::string decimal_p_romano(int decimal) {
+  std::string romano = "";
+  std::vector<std::pair<int, std::string>> algarismos = {
+    {1000, "M"},
+    {900, "CM"},
+    {500, "D"},
+    {400, "CD"},
+    {100, "C"},
+    {90, "XC"},
+    {50, "L"},
+    {40, "XL"},
+    {10, "X"},
+    {9, "IX"},
+    {5, "V"},
+    {4, "IV"},
+    {1, "I"}
+  };
 
-std::string valor_decimal(int algarismo) {
-  switch (algarismo) {
-    case 1:
-      return "I";
-    case 5:
-      return "V";
-    case 10:
-      return "X";
-    case 50:
-      return "L";
-    case 100:
-      return "C";
-    case 500:
-      return "D";
-    case 1000:
-      return "M";
-    default:
-      return 0;
+  for (auto& par : algarismos) {
+    while (decimal >= par.first) {
+      romano += par.second;
+      decimal -= par.first;
+    }
   }
+
+  return romano;
 }
 
 int romanos_para_decimal(const char* num_romano) {
-  int resultado = 0;
-  int tamanho = std::string(num_romano).length();
-  int repeticao = 1;
-
-  for (int i = 0; i < tamanho; i++) {
-    int valor_atual = valor_romano(num_romano[i]);
-      if (valor_atual == -1) {
-        return -1;
-      }
-
-        if (i > 0 && num_romano[i] == num_romano[i - 1]) {
-            repeticao++;
-            if (repeticao > 3) {
-                return -1;
-            }
-        } else {
-            repeticao = 1;
-        }
-
-        if (i < tamanho - 1 && valor_romano(num_romano[i + 1]) > valor_atual) {
-            resultado -= valor_atual;
-        } else {
-            resultado += valor_atual;
-        }
+  for (int i = 0; i < 3000; i++) {
+    if (std::string(num_romano) == decimal_p_romano(i + 1)) {
+      return i + 1;
     }
-
-    std::string romano_convertido;
-    for (int i = 0; i < resultado; i++) {
-        romano_convertido += valor_decimal(resultado);
-    }
-    if (romano_convertido != num_romano) {
-        return -1;
-    }
-
-  return resultado;
+  }
+  return -1;
 }
